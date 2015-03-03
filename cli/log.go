61f003a814342cd4f765f40c410437f20780d289
Blake Gentry <blakesgentry@gmail.com>
2015-03-03 13:15:26 -0800

    WIP: CLI app logs
    Signed-off-by: Blake Gentry <blakesgentry@gmail.com>

--- cli/log.go
+++ cli/log.go
@@ -3,37 +3,50 @@ package main
 import (
 	"io"
 	"os"
+	"strconv"
 
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-docopt"
 	"github.com/flynn/flynn/controller/client"
-	"github.com/flynn/flynn/pkg/cluster"
 )
 
 func init() {
 	register("log", runLog, `
-usage: flynn log [options] <job>
+usage: flynn log [options]
 
-Stream log for a specific job.
+Stream log for an app.
 
 Options:
-	-s, --split-stderr  send stderr lines to stderr
-	-f, --follow        stream new lines after printing log buffer
+	-f, --follow         stream new lines after printing log buffer
+	-n, --number <lines> return at most n lines from the log buffer
+	-j, --job <id>       filter logs to a specific job ID
+	-s, --split-stderr   send stderr lines to stderr
 `)
 }
 
 func runLog(args *docopt.Args, client *controller.Client) error {
-	rc, err := client.GetJobLog(mustApp(), args.String["<job>"], args.Bool["--follow"])
+	lines := 0
+	if strlines := args.String["--number"]; strlines != "" {
+		var err error
+		if lines, err = strconv.Atoi(strlines); err != nil {
+			return err
+		}
+	}
+	rc, err := client.GetAppLog(mustApp(), lines, args.Bool["--follow"])
 	if err != nil {
 		return err
 	}
+	defer rc.Close()
+
 	var stderr io.Writer = os.Stdout
 	if args.Bool["--split-stderr"] {
 		stderr = os.Stderr
 	}
-	attachClient := cluster.NewAttachClient(struct {
-		io.Writer
-		io.ReadCloser
-	}{nil, rc})
-	attachClient.Receive(os.Stdout, stderr)
+
+	// TODO: don't want to copy raw bytes to stdout, want to deserialize the
+	// logaggregator client.Message structs and print the message portion with
+	// some Heroku-like display logic.
+	if _, err = io.Copy(os.Stdout, rc); err != nil {
+		return err
+	}
 	return nil
 }
